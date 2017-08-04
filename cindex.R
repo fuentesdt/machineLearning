cf8b41b039c86bffd7dd664a3d8c4b2cb3152e6d
gpauloski <gpauloski@yahoo.com>
2017-08-04 15:31:57 -0500

    Added some lines for converting char vectors to int factors

--- cindex.R
+++ cindex.R
@@ -3,17 +3,17 @@ library(compareC, quietly=TRUE)
 dataset <- read.csv("file:///home/gpauloski/git-repos/TACE/gmmdatamatrix_July_28.csv")
 models <- read.csv("file:///home/gpauloski/git-repos/TACE/modelPredictions.csv")
 
-#cat("\nBCLC:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_BCLC"]))
-#cat("\nCLIP:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_CLIP"]))
-#cat("\nTNM:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_TNM"]))
-#cat("\nOkuda:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_Okuda"]),"\n")
+#dataset[,"liver_BCLC"] <- as.numeric(factor(dataset[,"liver_BCLC"], levels=levels(factor(dataset[,"liver_BCLC"]))))
+#dataset[,"liver_CLIP"] <- as.numeric(factor(dataset[,"liver_CLIP"], levels=levels(factor(dataset[,"liver_CLIP"]))))
+#dataset[,"liver_TNM"] <- as.numeric(factor(dataset[,"liver_TNM"], levels=levels(factor(dataset[,"liver_TNM"]))))
+#dataset[,"liver_Okuda"] <- as.numeric(factor(dataset[,"liver_Okuda"], levels=levels(factor(dataset[,"liver_Okuda"]))))
 
 cindex <- function(time, pred) {
 	nobs <- length(time)
 	conc <- 0
 	tie <- 0
-	npairs <- 0
-
+	npairs <- 0	
+	
 	for(i in 1:nobs) {
 		for(j in 1:nobs) {
 			if(i==j) break
@@ -31,6 +31,15 @@ cindex <- function(time, pred) {
 	return((conc+(0.5*tie))/npairs)
 } 
 
+#cat("\nBCLC:", cindex(dataset[,"liver_TTP"],dataset[,"liver_BCLC"]))
+#cat("\nCLIP:", cindex(dataset[,"liver_TTP"],dataset[,"liver_CLIP"]))
+#cat("\nTNM:", cindex(dataset[,"liver_TTP"],dataset[,"liver_TNM"]))
+#cat("\nOkuda:", cindex(dataset[,"liver_TTP"],dataset[,"liver_Okuda"]),"\n")
+
+#cat("\nBCLC:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_BCLC"]))
+#cat("\nCLIP:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_CLIP"]))
+#cat("\nTNM:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_TNM"]))
+#cat("\nOkuda:", estC(dataset[,"liver_TTP"],dataset[,"liver_CensorStatus"],dataset[,"liver_Okuda"]),"\n")
 
 output <- data.frame(model=as.character(seq(1,ncol(models[,-1]),1)))
 output <- cbind(output, c_index=seq(1,ncol(models[,-1]),1))
